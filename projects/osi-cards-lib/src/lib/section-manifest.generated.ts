fbe56c99a1ea0f0c51de03cf647e73256edb06ef
Arthur Mariani <arthur.mariani@caramail.fr>
2025-12-12 12:12:36 +0100

    chore(release): v1.5.19 [publish]

--- projects/osi-cards-lib/src/lib/section-manifest.generated.ts
+++ projects/osi-cards-lib/src/lib/section-manifest.generated.ts
@@ -783,8 +783,8 @@ export function getSectionsRequiringExternalLibs(): Map<string, string[]> {
  * Manifest metadata
  */
 export const MANIFEST_META = {
-  generatedAt: '2025-12-12T11:11:00.899Z',
-  registryVersion: '1.5.18',
+  generatedAt: '2025-12-12T11:12:18.177Z',
+  registryVersion: '1.5.19',
   totalSections: 22,
   publicSections: 22
 };
